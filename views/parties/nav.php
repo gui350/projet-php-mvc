ae995f0d0aa1fd0266f450f015cdc38b5c6adec6
VANG <vangguillaume@gmail.com>
2022-02-22 13:58:29 +0100

    commit exo 7

--- views/parties/nav.php
+++ views/parties/nav.php
@@ -1,7 +1,21 @@
 <nav class="nav my-2 py-2 justify-content-center">
     <a class="nav-link" href="index.php?route=home">Acceuil</a>
     <a class="nav-link" href="index.php?route=liste-articles">Liste des articles</a>
+
+
+    <?php if (!empty($_SESSION['identifiant']) && $_SESSION['identifiant'] == 'admin') : ?>
     <a class="nav-link" href="index.php?route=ajouter-articles">Ajouter un articles</a>
+    <?php endif; ?>
+
+
+
+    <?php if (empty($_SESSION['identifiant'])) : ?>
+        <a class="nav-link" href="index.php?route=connexion">Connexion</a>
+        
+    <?php else : ?>
+        <a class="nav-link" href="index.php?route=deconnexion">Déconnexion</a>
+    <?php endif; ?>
+
 
 
 </nav>
